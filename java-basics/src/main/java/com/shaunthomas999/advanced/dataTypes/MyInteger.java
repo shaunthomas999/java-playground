86ce682a54a89dda181121eb51a4e6572af242c8
Shaun Thomas <shaunthomas999@gmail.com>
2023-08-21 22:37:08 +0200

    Version bump and minor changes

--- java-basics/src/main/java/com/shaunthomas999/advanced/dataTypes/MyInteger.java
+++ java-basics/src/main/java/com/shaunthomas999/advanced/dataTypes/MyInteger.java
@@ -2,11 +2,12 @@ package com.shaunthomas999.advanced.dataTypes;
 
 public class MyInteger {
 
-    public static void main(String[] args) {
-        int someNumber = 2+3;
+  public static void main(String[] args) {
+    int someNumber = 2 + 3;
 
-        Integer someNumberObj = new Integer(someNumber);
+//    Integer someNumberObj = Integer.valueOf(someNumber);
+    Integer someNumberObj = someNumber;
 
-        System.out.println(someNumberObj);
-    }
+    System.out.println(someNumberObj);
+  }
 }
