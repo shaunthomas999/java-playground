654c313ccf2c7c190c7b690868583d0b81c6ea7a
Shaun Thomas <shaunthomas999@gmail.com>
2024-04-13 18:30:08 +0200

    Java 21

---
+++ java-21/src/main/java/com/shaunthomas999/java21/A_VirtualThreadsExamples.java
@@ -0,0 +1,24 @@
+package com.shaunthomas999.java21;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class A_VirtualThreadsExamples {
+
+  /**
+   * Below code is not working!!
+   */
+  public void example_01() {
+    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+      // 10_000
+      IntStream.range(0, 20).forEach(i -> {
+        executor.submit(() -> {
+//          System.out.println("virtual-thread #" + i);
+          Thread.sleep(Duration.ofMillis(100));
+          return i;
+        });
+      });
+    }  // executor.close() is called implicitly, and waits
+  }
+}
---
+++ java-21/src/test/java/com/shaunthomas999/java21/A_VirtualThreadsExamplesTest.java
@@ -0,0 +1,14 @@
+package com.shaunthomas999.java21;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class A_VirtualThreadsExamplesTest {
+
+  @Test
+  void example_01_Test() {
+    assertTrue(true);
+//    example_01();
+  }
+}
