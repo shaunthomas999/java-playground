b0b2265edd1f8ea62ef356d3f20fd11099a2f627
Shaun Thomas <shaunthomas999@gmail.com>
2025-02-07 00:13:00 +0100

    Updates

---
+++ java-basics/src/main/java/com/shaunthomas999/advanced/collections/common/JavaCollections.java
@@ -0,0 +1,15 @@
+package com.shaunthomas999.advanced.collections.common;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class JavaCollections {
+
+  public static void main(String[] args) {
+
+    var intList = Arrays.asList(1, 2, 3, 4, 5);
+
+    Collections.sort(intList);
+
+  }
+}
--- java-basics/src/main/java/com/shaunthomas999/advanced/currency/CurrencyPlay.java
+++ java-basics/src/main/java/com/shaunthomas999/advanced/currency/CurrencyPlay.java
@@ -5,6 +5,8 @@ import java.util.Currency;
 public class CurrencyPlay {
 
     public static void main(String[] args) {
+        System.out.println(Currency.getInstance("EUR").toString());
+        System.out.println(Currency.getInstance("EURO").toString());
         System.out.println(Currency.getInstance("EUROO").toString());
     }
 }
