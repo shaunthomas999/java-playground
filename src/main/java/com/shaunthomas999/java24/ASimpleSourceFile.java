c62f3a180b4d435b55ac15e7d79ac9354c76fcf1
Shaun Thomas <shaunthomas999@gmail.com>
2025-03-24 11:38:43 +0100

    Simple Source File

--- src/main/java/com/shaunthomas999/java24/ASimpleSourceFile.java
+++ src/main/java/com/shaunthomas999/java24/ASimpleSourceFile.java
@@ -1,10 +1,19 @@
+package com.shaunthomas999.java24;
+
 // FIXME - Not working in Intellij
 //java: java.lang.ExceptionInInitializerError
 //com.sun.tools.javac.code.TypeTag :: UNKNOWN
+
 /**
  * Simple source file (implicitly declared class)
  * java.io.IO - contains console println and readln methods implicitly added in simple source files
  */
-void main() {
-  println("Hello Shaun!");
+public class ASimpleSourceFile {
+  public static void main(String[] args) {
+    System.out.println("Hello Shaun!");
+  }
 }
+
+//void main() {
+//  println("Hello Shaun!");
+//}
